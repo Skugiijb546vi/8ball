e92c31011f213d66166fdb499d4415a644afbb10
Sarko Akram <sarkoakram333@gmail.com>
2026-03-08 01:33:26 +0300

    First upload for 8ball

---
+++ UltimateEngine External/app/src/main/jni/app/Decl.h
@@ -0,0 +1,6 @@
+#ifndef decl_h
+#define decl_h
+
+static float Speed = 1;
+//@tg: @xvaluex.
+#endif
---
+++ UltimateEngine External/app/src/main/jni/app/Pointers.h
@@ -0,0 +1,12 @@
+#ifndef Pointers_h
+#define Pointers_h
+//@tg: @xvaluex.
+#include "Math.h"
+
+
+    
+
+
+    
+
+#endif 
